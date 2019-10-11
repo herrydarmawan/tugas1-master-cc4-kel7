4d06a58e060c7370cf846b28380508c32c75b4f2
Herry Darmawan <hdarmawan123@gmail.com>
2019-10-11 16:30:30 +0700

    initial reposito

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<center>
+		<h1>MASTER</h1>
+		<a href="https://tugas1-101162xx-08.herokuapp.com/">Herry</a>
+		<a href="https://tugas1-101161xx-04.herokuapp.com/">Zacky</a>
+		<a href="https://tugas1-101161xx-05.herokuapp.com/">Miggy</a>
+</body>
+</html>
