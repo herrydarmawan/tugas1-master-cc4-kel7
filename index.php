b93d0a35747b0b5529eb23e7ca836aaaa639f3e2
Herry Darmawan <hdarmawan123@gmail.com>
2019-10-11 17:01:30 +0700

    Nambah Kumar

--- index.php
+++ index.php
@@ -9,5 +9,6 @@
 		<a href="https://tugas1-101162xx-08.herokuapp.com/">Herry</a>
 		<a href="https://tugas1-101161xx-04.herokuapp.com/">Zacky</a>
 		<a href="https://tugas1-101161xx-05.herokuapp.com/">Miggy</a>
+		<a href="https://tugas1-101162xx-12.herokuapp.com/">Kumar</a>
 </body>
 </html>
