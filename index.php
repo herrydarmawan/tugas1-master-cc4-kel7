5d9905e88f5387c8becf905b079cd6d558e1d6d1
Herry Darmawan <hdarmawan123@gmail.com>
2019-10-12 12:41:37 +0700

    tambah

--- index.php
+++ index.php
@@ -10,6 +10,6 @@
 		<a href="https://tugas1-101161xx-04.herokuapp.com/">Zacky</a>
 		<a href="https://tugas1-101161xx-05.herokuapp.com/">Miggy</a>
 		<a href="https://tugas1-101162xx-12.herokuapp.com/">Kumar</a>
-		<a href="#">Tugas Review</a>
+		<a href="https://drive.google.com/open?id=1oleTfvHU7N0M-u3smZBXcB63B2YPDBuP">Tugas Review</a>
 </body>
 </html>
