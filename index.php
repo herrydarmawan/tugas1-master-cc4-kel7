ad22d4d7a4b317c1b204d5a7ccca7d987d5b00ea
Herry Darmawan <hdarmawan123@gmail.com>
2019-10-13 15:10:38 +0700

    ganti baris

--- index.php
+++ index.php
@@ -7,9 +7,13 @@
 	<center>
 		<h1>MASTER</h1>
 		<a href="https://tugas1-101162xx-08.herokuapp.com/">Herry</a>
+		<br>
 		<a href="https://tugas1-101161xx-04.herokuapp.com/">Zacky</a>
+		<br>
 		<a href="https://tugas1-101161xx-05.herokuapp.com/">Miggy</a>
+		<br>
 		<a href="https://tugas1-101162xx-12.herokuapp.com/">Kumar</a>
+		<br>
 		<a href="https://drive.google.com/file/d/1q3I3rumAcJlg6aDkD7pzECYTGQcS9Fom/view?usp=sharing">Tugas Review</a>
 </body>
 </html>
